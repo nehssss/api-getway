d27f694f8eabd42aade6a1df24e19f4709894f98
nehssss <562410105@qq.com>
2023-05-04 00:12:24 -0500

    init api-gateway

---
+++ src/test/java/com/haitaos/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitaos.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
